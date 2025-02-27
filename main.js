aa29a4cb7345b75f214cd95ecf2af7fd8b653c4e
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-27 18:02:15 +0100

    add css

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 /* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
 const listEl = document.querySelector(".list")
+const btnEl = document.querySelector(".btn")
+const arrowEl = document.querySelector(".arrow")
+const refreshEl = document.querySelector("refresh")
 
 
-for(let i = 0; i < 10; i++){
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => response.json())
-    .then(data => {
-    console.log(data.response);
-   
-    const elementLi = document.createElement("li")
-    elementLi.innerHTML = data.response
-    listEl.appendChild(elementLi)
+
+btnEl.addEventListener("click", function(){
+    for(let i = 0; i < 10; i++){
+        listEl.innerHTML = ""
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => response.json())
+        .then(data => {
+        console.log(data.response);
+
+        const elementLi = document.createElement("li")
+        elementLi.innerHTML = data.response
+        listEl.appendChild(elementLi)
+        arrowEl.classList.add("d-none")
+
+    })
+    }
+    
+
 })
-}
+
 
 
 
